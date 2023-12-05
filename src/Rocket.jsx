ecfdfa7c0671e30d13ff58b0bb42a575274f4859
Dennis Hickox <frozzel@me.com>
2023-12-05 10:54:56 -0500

    added name to scene

--- src/Rocket.jsx
+++ src/Rocket.jsx
@@ -1,19 +1,34 @@
-import 'aframe';
+import  { useEffect } from 'react';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 import rocketModel from './models/rocket.glb';
 import Prototypes from 'prop-types';
 
 
 function Rocket({x, y, z}) {
-    const loader = new GLTFLoader();
-    loader.load(rocketModel, (d) => {
-        const entity = document.getElementById('rocket');
-        entity.object3D.add(d.scene);
-    })
 
-    return(
-        <a-entity id="rocket" position={`${x} ${y} ${z}`} scale="15 15 15" rotation="0 0 40"></a-entity>
-    );
+useEffect(() => {
+    const loader = new GLTFLoader();
+    const rocketEntity = document.getElementById('rocket');
+
+    if (rocketEntity) {
+      loader.load(rocketModel, (d) => {
+        rocketEntity.object3D.add(d.scene);
+
+
+        // Position Animation (circular path)
+        rocketEntity.setAttribute('animation__position', {
+          property: 'position',
+          dur: 20000, // Animation duration in milliseconds
+          easing: 'linear', // You can change the easing function
+          from: `${x} ${y} ${z}`, // Initial position
+          to: `${x - 1000} ${y + 1000} ${z-1}`, // Destination position
+          loop: true, // Loop the position animation
+        });
+      });
+    }
+  }, []);
+
+  return <a-entity id="rocket" position={`${x} ${y} ${z}`} scale="15 15 15" rotation={`0 0 ${z - 200}`}></a-entity>;
 }
 
 Rocket.Prototypes = {
---
+++ src/Stage.jsx
@@ -0,0 +1,25 @@
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import rocketModel from './models/scene.glb';
+import Prototypes from 'prop-types';
+
+
+function Stage({x, y, z}) {
+    const loader = new GLTFLoader();
+    loader.load(rocketModel, (d) => {
+        const entity = document.getElementById('stage');
+        entity.object3D.add(d.scene);
+    })
+
+    return(
+        <a-entity id="stage" position={`${x} ${y} ${z}`} scale="4 4 4" rotation="0 90 0"></a-entity>
+    );
+}
+
+Stage.Prototypes = {
+    x: Prototypes.number,
+    y: Prototypes.number,
+    z: Prototypes.number
+}
+
+
+export default Stage;
